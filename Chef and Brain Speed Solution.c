48638d0b0662464cd8694ac5ec3804ed7b4d5356
Akashat Ojha <118652614+Akashat-02-Dev@users.noreply.github.com>
2022-11-20 13:18:28 +0530

    Add files via upload

---
+++ Chef and Brain Speed Solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int X, Y;
+	scanf("%d",&X);
+	scanf("%d",&Y);
+	if(Y>X){
+	    printf("YES");
+	}
+	else{
+	    printf("NO");
+	}
+	return 0;
+}
+
